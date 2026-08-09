50345241eb44691e701c90cbdd98f32883c6ee3e
Vaibhav Samdani <83386703+Vaibhav-Samdani@users.noreply.github.com>
2026-08-10 04:09:25 +0530

    Add: surrounded-regions - submission-0

---
+++ Data Structures & Algorithms/surrounded-regions/submission-0.java
@@ -0,0 +1,53 @@
+class Solution {
+    public void solve(char[][] board) {
+
+        int m = board.length;
+        int n = board[0].length;
+
+        for (int i = 0; i < n; i++) {
+            if (board[0][i] == 'O') {
+                dfs(board, 0, i);
+            }
+            if (board[m - 1][i] == 'O') {
+                dfs(board, m - 1, i);
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            if (board[i][0] == 'O') {
+                dfs(board, i, 0);
+            }
+            if (board[i][n - 1] == 'O') {
+                dfs(board, i, n - 1);
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+                if (board[i][j] == 'P') {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+
+    }
+
+    void dfs(char[][] board, int i, int j) {
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != 'O')
+            return;
+
+        board[i][j] = 'P';
+
+        int[][] dir = { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
+        for (int[] d : dir) {
+            int nr = i + d[0];
+            int nc = j + d[1];
+
+            dfs(board, nr, nc);
+
+        }
+    }
+}
