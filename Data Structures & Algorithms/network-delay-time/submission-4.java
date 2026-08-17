7a16c2a0c6e2ee24f32b8edc8473edd77eb89f2e
Vaibhav Samdani <83386703+Vaibhav-Samdani@users.noreply.github.com>
2026-08-18 02:39:36 +0530

    Add: network-delay-time - submission-4

---
+++ Data Structures & Algorithms/network-delay-time/submission-4.java
@@ -0,0 +1,42 @@
+class Solution {
+    class Pair {
+        int node;
+        int time;
+        Pair(int node, int time) {
+            this.node = node;
+            this.time = time;
+        }
+    }
+    public int networkDelayTime(int[][] times, int n, int k) {
+        int V = n + 1;
+
+        int[] result = new int[V];
+        Arrays.fill(result, Integer.MAX_VALUE);
+
+        result[k] = 0;
+
+        for (int i = 0; i <= V - 1; i++) {
+            boolean updated = false;
+            for (int[] time : times) {
+                if (result[time[0]] != Integer.MAX_VALUE
+                    && result[time[0]] + time[2] < result[time[1]]) {
+                    updated = true;
+                    result[time[1]] = result[time[0]] + time[2];
+                }
+            }
+
+            if (!updated)
+                break;
+        }
+
+        int max = 0;
+
+        for (int i = 1; i < result.length; i++) {
+            if (result[i] == Integer.MAX_VALUE) {
+                return -1;
+            }
+            max = Math.max(max, result[i]);
+        }
+        return max;
+    }
+}
